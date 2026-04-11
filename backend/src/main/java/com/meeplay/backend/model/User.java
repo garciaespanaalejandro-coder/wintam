6e14a868e154f26977449aa6ec8bd35faee43034
Alejandro <mralexsito@gmail.com>
2026-04-11 16:43:27 +0200

    modelo user y role creada

---
+++ backend/src/main/java/com/meeplay/backend/model/User.java
@@ -0,0 +1,71 @@
+package com.meeplay.backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name= "user")
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "pasword_hash", nullable = false)
+    private String passwordHash;
+
+    private LocalDate birhdate;
+    private String description;
+
+    @Column(name="verification_code")
+    private String verificationCode;
+
+    @Column(name="is_verified", nullable = false)
+    @Builder.Default
+    private Boolean isVerified= false;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Builder.Default
+    private Role role = Role.USER;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Integer karma=50;
+
+    @Column(name="created_at", nullable= false, updatable = false)
+    @Builder.Default
+    private LocalDateTime createdAt= LocalDateTime.now();
+
+    // UserDetails
+    @Override public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+    @Override public String getPassword() { return passwordHash; }
+    @Override public String getUsername() { return email; }
+    @Override public boolean isEnabled() { return isVerified; }
+}
