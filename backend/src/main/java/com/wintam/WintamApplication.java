d6fa2d2c029b5d432ec00df9a813a31e245e4aca
Alejandro <mralexsito@gmail.com>
2026-04-23 17:15:31 +0200

    eliminar DotenvConfig.java y añadir plugin para .env

--- backend/src/main/java/com/wintam/WintamApplication.java
+++ backend/src/main/java/com/wintam/WintamApplication.java
@@ -1,6 +1,5 @@
 package com.wintam;
 
-import com.wintam.config.DotenvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WintamApplication {
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(WintamApplication.class);
-        app.addInitializers(new DotenvConfig());
-        app.run(args);
+        SpringApplication.run(WintamApplication.class, args);
     }
 }
